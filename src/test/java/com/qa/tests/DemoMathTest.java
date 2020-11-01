ac22947ef9699391d8649c5ba35cd5501e94c7a1
manikrishnabynv <yeswanth159@gmail.com>
2020-11-01 22:13:54 +0530

    New Code

---
+++ src/test/java/com/qa/tests/DemoMathTest.java
@@ -0,0 +1,36 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoMathTest {
+	
+	int a=10;
+	int b=20;
+	
+	@Test
+	public void sum() {
+		System.out.println("SUM");
+		Assert.assertEquals(30, a+b);
+	}
+	
+	@Test
+	public void sub() {
+		System.out.println("SUB");
+		Assert.assertEquals(10, b-a);
+	}
+	
+	@Test
+	public void Mul() {
+		System.out.println("MUL");
+		Assert.assertEquals(200, a*b);
+	}
+	
+	@Test
+	public void Div() {
+		System.out.println("DIV");
+		Assert.assertEquals(2, b/a);
+	}
+	
+
+}
